ea68870869e748ac5f6dfead5f315c756894b4e0
Supercat852s <anghel.paul@scoalagimnaziala195.ro>
2022-11-06 21:52:02 +0200

    A cat has encrypted your file!

--- PFM_0.5.py
+++ PFM_0.5.py
@@ -1,34 +1,38 @@
 import os
+from cryptography.fernet import Fernet
 
 # Prints the introduction.
-print("Welcome to Python File Manager 0.4!!")
+print("Welcome to Python File Manager 0.5!!")
 print("Type 'help' for help.")
 print("Do 'p' for patch notes.")
+print("Version splash: Why not encrypt your files?")
 
 # Defines the help command.
 def doHelp():
     print("Welcome to help command!")
     print("Available commands:")
     print("[p] List new patch notes.")
-    print("[ls] List the contents of the current directory.")
+    print("[ls] List the c of the current directory.")
     print("[ta] Add text to an existing text file.")
     print("[tw] Overwrite an existing text file.")
-    print("[cat] Shows the contents of a text file.")
+    print("[cat] Shows the c of a text file.")
     print("[mk] Create a file.")
     print("[mkdir] Create a directory.")
     print("[rm] Remove a file.")
     print("[rmdire] Remove a empty directory.")
+    print("[ec] Encrypt a file of your choice.")
+    print("[dc] Decrypt a encrypted file.")
     print("[exit] Exit the program.")
     doStartup()
 
 # Defines the p command.
 def doP():
     print("Welcome to PFM patch notes!")
-    print("Version: 0.4")
-    print("0.4 patch notes:")
-    print("-Added full compatability with files out of external folders.")
-    print("-A function in the code was relocated.")
-    print("-Most commands got new phrases!")
+    print("Version: 0.5")
+    print("0.5 patch notes:")
+    print("-The encrypt command was added.")
+    print("-The decrypt command was added.")
+    print("-The version splash was added.")
 
 # Defines the ls command.
 def doLs1():
@@ -117,34 +121,85 @@ def doRmdire():
     print("Directory removed.")
     doStartup()
 
+# Defines the ec command.
+def doEc():
+    print("What file do you want encrypted?(Also specify the path.)")
+    i = input(">")
+    file = i
+    with open(file, "rb") as r:
+        c = r.read()
+    key = Fernet.generate_key()
+    print("Please copy this key for later decryption: " + str(key))
+    if os.path.exists("thekey.key"):
+        os.remove("thekey.key")
+    with open("thekey.key", "x") as x:
+        with open("thekey.key", "wb") as w:
+            w.write(key)
+    ce = Fernet(key).encrypt(c)
+    with open(file, "wb") as w:
+        w.write(ce)
+    print("File encrypted succesfully.")
+    doStartup()
+    
+    # c_e = Fernet(password).encrypt(c)
+    # with open(file, "wb") as wb:
+    #     wb.write(c_e)
+
+# Defines the dc command.
+def doDc():
+    print("What file do you want decrypted?[Specify the path!]")
+    i = input(">")
+    file = i
+    with open(i, "rb") as rb:
+        c = rb.read()
+    with open("thekey.key", "rb") as rb:
+        key = rb.read()
+    print("What's the key?")
+    i = input(">")
+    if str(i) == str(key):
+        with open(file, "w") as wb:
+            cd = Fernet(key).decrypt(c)
+            wb.write(str(cd))
+        print("File decrypted.")
+    else:
+        print("--WRONG KEY--")
+        print("The whole command was rerunned.")
+        print("Next time enter the correct key!")
+        doDc()
+    if os.path.exists("thekey.key"):
+        os.remove("thekey.key")
+    doStartup()
+
 # Defines the startup funcion.
 def doStartup():
     i = input(">")
-
-    match i:
-        case "p":
-            doP()
-        case "ls":
-            doLs2()
-        case "ta":
-            doTa()
-        case "tw":
-            doTw()
-        case "cat":
-            doCat()
-        case "mk":
-            doMk()
-        case "mkdir":
-            doMkdir()
-        case "rm":
-            doRm()
-        case "rmdire":
-            doRmdire()
-        case "help":
-            doHelp()
-        case "exit":
-            print("Thank you for using PFM!")
-            exit()
+    if i == "p":
+        doP()
+    if i == "ls":
+        doLs2()
+    if i == "ta":
+        doTa()
+    if i == "tw":
+        doTw()
+    if i == "cat":
+        doCat()
+    if i == "mk":
+        doMk()
+    if i == "mkdir":
+        doMkdir()
+    if i == "rm":
+        doRm()
+    if i == "rmdire":
+        doRmdire()
+    if i == "ec":
+        doEc()
+    if i == "dc":
+        doDc()
+    if i == "help":
+        doHelp()
+    if i == "exit":
+        print("Thank you for using PFM!")
+        exit()    
     doStartup()
 
 # Starts the program.
